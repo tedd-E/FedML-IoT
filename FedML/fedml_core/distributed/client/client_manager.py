9f6b35dbd6dbdd86e022fae8db7989a1a0ad43bd
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-02-01 18:26:33 -0800

    Update client_manager.py

--- FedML/fedml_core/distributed/client/client_manager.py
+++ FedML/fedml_core/distributed/client/client_manager.py
@@ -3,15 +3,14 @@ from abc import abstractmethod
 
 from mpi4py import MPI
 
-from ..communication.gRPC.grpc_comm_manager import GRPCCommManager
 from ..communication.message import Message
 from ..communication.mpi.com_manager import MpiCommunicationManager
 from ..communication.mqtt.mqtt_comm_manager import MqttCommManager
 from ..communication.observer import Observer
-from ..communication.trpc.trpc_comm_manager import TRPCCommManager
 
 
 class ClientManager(Observer):
+
     def __init__(self, args, comm=None, rank=0, size=0, backend="MPI"):
         self.args = args
         self.size = size
@@ -20,18 +19,10 @@ class ClientManager(Observer):
         if backend == "MPI":
             self.com_manager = MpiCommunicationManager(comm, rank, size, node_type="client")
         elif backend == "MQTT":
-            HOST = "127.0.0.1"
+            HOST = "172.20.10.9"
             # HOST = "broker.emqx.io"
             PORT = 1883
             self.com_manager = MqttCommManager(HOST, PORT, client_id=rank, client_num=size - 1)
-        elif backend == "GRPC":
-            HOST = "0.0.0.0"
-            PORT = 50000 + rank
-            self.com_manager = GRPCCommManager(
-                HOST, PORT, ip_config_path=args.grpc_ipconfig_path, client_id=rank, client_num=size - 1
-            )
-        elif backend == "TRPC":
-            self.com_manager = TRPCCommManager(args.trpc_master_config_path, process_id=rank, world_size=size)
         else:
             self.com_manager = MpiCommunicationManager(comm, rank, size, node_type="client")
         self.com_manager.add_observer(self)
@@ -71,9 +62,3 @@ class ClientManager(Observer):
         logging.info("__finish server")
         if self.backend == "MPI":
             MPI.COMM_WORLD.Abort()
-        elif self.backend == "MQTT":
-            self.com_manager.stop_receive_message()
-        elif self.backend == "GRPC":
-            self.com_manager.stop_receive_message()
-        elif self.backend == "TRPC":
-            self.com_manager.stop_receive_message()
